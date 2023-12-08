601c91e3f72f091459d0c7abbbc9242457962635
Alex Kutsupis <axak55@outlook.com>
2023-12-07 22:14:40 -0500

    Remove "dropna()" that was preventing output. Add options.

--- EU_Transport_Modes/app.R
+++ EU_Transport_Modes/app.R
@@ -1,3 +1,4 @@
+source("shiny_keys.R")
 # Source:
 # https://ec.europa.eu/eurostat/databrowser/view/nrg_ind_ren/default/table?lang=en
 
@@ -12,7 +13,7 @@ library(shiny)
 data <- read_csv("nrg_ind_ren_page_linear.csv")
 nuts_sf <- st_read('nuts_shapefile.shp')
 
-source("shiny_keys.R")
+
 connect()
 
 # Filter to the country level
@@ -28,13 +29,13 @@ merged_data <- merge(nuts_sf_filtered, data, by.x = "CountryCode", by.y = "geo",
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("EU Freight Movement Visualization"),
+  titlePanel("Share of Renewable Energy"),
   sliderInput("year_slider", "Select Year:",
               min = min(merged_data$TIME_PERIOD, na.rm = TRUE),
               max = max(merged_data$TIME_PERIOD, na.rm = TRUE),
               value = min(merged_data$TIME_PERIOD, na.rm = TRUE),
-              step = 1, ticks = FALSE, sep = ','),
-  leafletOutput("map")
+              step = 1, ticks = FALSE, sep = ''),
+  leafletOutput("map", width = "100%", height = "800px")
 )
 
 # Define Server
@@ -42,16 +43,17 @@ server <- function(input, output, session) {
   # Filter data based on the selected year and drop NAs
   filtered_data <- reactive({
     req(input$year_slider)
-    na.omit(merged_data[merged_data$TIME_PERIOD == input$year_slider, ])
+    merged_data[merged_data$TIME_PERIOD == input$year_slider, ]
   })
   
   colorpal <- reactive({
-    colorNumeric("Blues", merged_data$OBS_VALUE)
+    colorNumeric("Greens", merged_data$OBS_VALUE)
   })
   
   # Create leaflet map with static elements
   output$map <- renderLeaflet({
     leaflet() %>%
+      setView(lng = 10, lat = 51, zoom = 3) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       addPolygons(data = filtered_data(),
                   fillColor = colorpal()(filtered_data()$OBS_VALUE),
@@ -59,7 +61,6 @@ server <- function(input, output, session) {
                   weight = 2,
                   color = "white",
                   popup = ~paste("Country: ", filtered_data()$CountryCode, "<br>",
-                                 "Freight Mode: ", filtered_data()$tra_mode, "<br>",
                                  "Percent: ", filtered_data()$OBS_VALUE)
       )
   })
@@ -73,14 +74,12 @@ server <- function(input, output, session) {
       clearShapes() %>%
       addPolygons(fillColor = ~pal(OBS_VALUE), fillOpacity = 0.6,
                   weight = 2, color = "white",
-                  popup = ~paste("Country: ", CountryCode, "<br>",
-                                 "Freight Mode: ", tra_mode, "<br>",
-                                 "Percent: ", OBS_VALUE)
+                  popup = ~paste("Country: ", filtered_data()$CountryCode, "<br>",
+                                 "Percent: ", filtered_data()$OBS_VALUE)
       )
   })
 }
 
 # Run the Shiny app
 shinyApp(ui, server)
-
 
